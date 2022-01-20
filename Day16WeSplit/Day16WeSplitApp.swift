260cf67c636e3df16b7498bdafa649470468db0b
Ceren <ctassin@geobilgi.com>
2022-01-20 23:11:57 +0300

    Initial Commit

---
+++ Day16WeSplit/Day16WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  Day16WeSplitApp.swift
+//  Day16WeSplit
+//
+//  Created by Ceren TAŞSIN on 20.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct Day16WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
